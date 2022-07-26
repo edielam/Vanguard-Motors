70230893b23433211389209db6a0f8250761f1e6
edielam <edguard738@gmail.com>
2022-07-26 13:25:18 +0000

    data-jpa resolved

--- MicroservicesAndAPIs/pricing-service/src/main/resources/import.sql
+++ MicroservicesAndAPIs/pricing-service/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO price (currency, vehicle_id, price) VALUES ('NG',  1, 14998.84);
-INSERT INTO price (currency, vehicle_id, price) VALUES ('XAF', 2, 8784.998);
-INSERT INTO price (currency, vehicle_id, price) VALUES ('EUR', 3, 78367.99);
-INSERT INTO price (currency, vehicle_id, price) VALUES ('EUR', 2, 87583.99);
+INSERT INTO price (currency, vehicle_id, price) VALUES ('GHS',  1, 14998.84);
+INSERT INTO price (currency, vehicle_id, price) VALUES ('GHS', 2, 8784.998);
+INSERT INTO price (currency, vehicle_id, price) VALUES ('GHS', 3, 78367.99);
+INSERT INTO price (currency, vehicle_id, price) VALUES ('GHS', 2, 87583.99);
